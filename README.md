34f8ba4085034abe3a112351b542af9aba5dda51
ashermanwmf <shermanandrew50@gmail.com>
2016-12-13 14:46:41 -0800

    clean up and added read me

---
+++ README.md
@@ -0,0 +1,4 @@
+# Getting Started
+
+1. npm install
+2. Open index.html in web browser
--- app.spec.js
+++ app.spec.js
@@ -1,30 +1,29 @@
 describe('App', () =>{
   beforeEach(module('app'));
 
-  describe('App', () =>{
-    let scope, $location, createController;
+  let scope, $location, createController;
 
-    beforeEach(inject(($rootScope, $controller, _$http_) =>{
-      scope = $rootScope.$new();
-      $http = _$http_;
+  beforeEach(inject(($rootScope, $controller, _$http_) =>{
+    scope = $rootScope.$new();
+    $http = _$http_;
 
-      createController = () =>{
-        return $controller('AppCtrl', {
-          '$scope': scope,
-          '$http': $http
-        });
-      };
-    }));
+    createController = () =>{
+      return $controller('AppCtrl', {
+        '$scope': scope,
+        '$http': $http
+      });
+    };
+  }));
 
-    it('controller should use factory to filter and return true for correct content', () =>{
-      let controller = createController();
+  it('controller should use factory to filter and return true for correct content', () =>{
+    let controller = createController();
 
-      //check content is defined
-      expect(scope.content).toBe('loading');
+    //check content is defined
+    expect(scope.content).toBe('loading');
 
-      //check filter works for options
-      expect(scope.priceFilter(2000)({maxPrice: 2000})).toBe(true);
-      expect(scope.priceFilter(0)({maxPrice: 950})).toBe(true);
-    });
+    //check filter works for options
+    expect(scope.priceFilter(2000)({maxPrice: 2000})).toBe(true);
+    expect(scope.priceFilter(2000)({maxPrice: 950})).toBe(false);
+    expect(scope.priceFilter(0)({maxPrice: 950})).toBe(true);
   });
 }); 
